0217c53d28516da4218c5fc6e4522af47c4108d2
Grzegorz Krajniak <gkrajniak@gmail.com>
2025-07-10 06:11:37 -0800

    fear: migrate iam to platform mesh (#8)
    * Refactor context handling and test enhancements
    
    - Replace `frameContext` with `portalContext` across services, components, and tests.
    - Enhance test cases with updated property checks, added mocks, and more robust assertions.
    - Remove unused imports and inline functions in components.
    - Introduce Jest configuration changes for better CSS handling in tests.
    
    * Remove deprecated modules and unused documentation.
    
    * Fetched latest changes from the iam-ui
    
    * Remove `AddYourTeamCard` component and related files
    
    - Delete `add-your-team-card` component, styles, tests, and SVG assets.
    - Remove associated dependencies and functionality in services and templates.
    - Clean up unused references to the component across the project.
    
    * Refactor and remove deprecated extensions module
    
    - Remove `extensions` services, queries, models, and related tests.
    - Update members page to use `iamClaimEntityUrl` dynamically for claiming entities.
    - Rename `ClaimProjectService` to `ClaimEntityService` and refactor logic for handling generic entities.
    - Clean up unused imports, services, and context properties across the application.
    
    * fix tests
    
    * fix tests
    
    * chore: provide scripts for production build
    
    * ci: update concurrently package version and add test script
    
    * chore update tests
    
    ---------
    
    Co-authored-by: Grzegorz Krajniak <grzegorz.krajniak@sap.com>
    Co-authored-by: Bastian Echterhölter <bastian.echterhoelter@sap.com>

--- projects/lib/src/lib/services/claim-project.service.spec.ts
+++ projects/lib/src/lib/services/claim-project.service.spec.ts
@@ -1,33 +1,48 @@
 import { IamLuigiContextService } from '../services';
 import { ClaimEntityService } from './claim-entity.service';
+import { MemberService } from './member.service';
 import { TestBed } from '@angular/core/testing';
 import { MockProvider } from 'ng-mocks';
-
-const projectId = 'abc';
-const frameBaseUrl = 'https://example.com';
+import { of } from 'rxjs';
 
 describe('ClaimProjectService', () => {
   let service: ClaimEntityService;
+  const entityId = 'abc';
+  const portalBaseUrl = 'https://example.com';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         ClaimEntityService,
         MockProvider(IamLuigiContextService, {
-          getContext: jest.fn().mockReturnValue({ projectId, frameBaseUrl }),
+          getContext: jest.fn().mockReturnValue({
+            portalBaseUrl,
+            entityContext: {
+              project: {
+                id: entityId,
+              },
+            },
+            portalContext: {
+              iamClaimEntityUrl:
+                'https://jira.tools.sap/secure/CreateIssueDetails!init.jspa?pid=106042&issuetype=10100&components=300068&summary=Claim+Project+Request+for+Project+${entityId}&description=h3.+Description%0ADescribe+why+you+want+to+claim+this+project%3A%0A%0AWho+should+be+added+as+an+owner+of+the+project%3F%0A%0Ah3.+Link+to+Project%0A${portalBaseUrl}%2Fprojects%2F${entityId}d%0A',
+            },
+          }),
+        }),
+        MockProvider(MemberService, {
+          currentEntity: () => of('project'),
         }),
       ],
     });
     service = TestBed.inject(ClaimEntityService);
   });
 
-  it('should open Jira URL to create a claim project ticket', () => {
+  it('should open Jira URL to create a claim project ticket', async () => {
     window.open = jest.fn();
 
-    service.claim();
+    await service.claim();
 
     expect(window.open).toHaveBeenCalledWith(
-      'https://jira.tools.sap/secure/CreateIssueDetails!init.jspa?pid=106042&issuetype=10100&components=300068&summary=Claim+Project+Request+for+Project+abc&description=h3.+Description%0ADescribe+why+you+want+to+claim+this+project%3A%0A%0AWho+should+be+added+as+an+owner+of+the+project%3F%0A%0Ah3.+Link+to+Project%0Ahttps%3A%2F%2Fexample.com%2Fprojects%2Fabc%0A',
+      'https://jira.tools.sap/secure/CreateIssueDetails!init.jspa?pid=106042&issuetype=10100&components=300068&summary=Claim+Project+Request+for+Project+abc&description=h3.+Description%0ADescribe+why+you+want+to+claim+this+project%3A%0A%0AWho+should+be+added+as+an+owner+of+the+project%3F%0A%0Ah3.+Link+to+Project%0Ahttps://example.com%2Fprojects%2Fabcd%0A',
       '_blank',
     );
   });
